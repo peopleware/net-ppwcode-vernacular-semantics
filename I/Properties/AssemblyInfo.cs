f347950a394c50851870cc13d22415a8d946c4d5
Jan Dockx <jan_dockx@peopleware.be>
2011-01-28 23:01:52 +0000

    adapted to Semantics
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Semantics/trunk@4645 463e38ab-4f4e-0410-907a-65d2b3888964

--- I/Properties/AssemblyInfo.cs
+++ I/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PPWCode.Vernacular.Exceptions.I")]
-[assembly: AssemblyDescription("Main code of the PPWCode Exceptions Vernacular")]
+[assembly: AssemblyTitle("PPWCode.Vernacular.Semantics.I")]
+[assembly: AssemblyDescription("Main code of the PPWCode Semantics Vernacular")]
 [assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a96a19e0-b3e8-4181-a2be-9518870d63d8")]
+[assembly: Guid("13987d00-d67a-4a1c-9168-b1a222fb2705")]
