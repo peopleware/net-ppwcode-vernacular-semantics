fe20a06f713577fd9aabc46455a03ca0807f3211
Jan Dockx <jan_dockx@peopleware.be>
2011-01-31 00:05:35 +0000

    Unit tests for AbstractRousseauObject
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Semantics/trunk@4653 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ PPWCode.Vernacular.Semantics.Test_I/AbstractRousseauObjectTest.cs
@@ -0,0 +1,109 @@
+﻿using PPWCode.Vernacular.Semantics.I;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PPWCode.Vernacular.Exceptions.I;
+
+namespace PPWCode.Vernacular.Semantics.Test_I
+{
+    [TestClass()]
+    public class AbstractRousseauObjectTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+
+        private AbstractRousseauObject[] m_Subjects;
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            m_Subjects = new AbstractRousseauObject[2];
+            m_Subjects[0] = new RousseauObject() { MockWild = false };
+            m_Subjects[1] = new RousseauObject() { MockWild = true };
+        }
+        
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            m_Subjects = null;
+        }
+
+        #endregion
+
+
+        [TestMethod()]
+        public void WildExceptionsTest()
+        {
+            foreach (AbstractRousseauObject aro in m_Subjects)
+            {
+                CompoundSemanticException cpe = aro.WildExceptions();
+            }
+        }
+
+        /// <summary>
+        ///A test for ThrowIfNotCivilized
+        ///</summary>
+        [TestMethod()]
+        public void ThrowIfNotCivilizedTest()
+        {
+            foreach (AbstractRousseauObject aro in m_Subjects)
+            {
+                try
+                {
+                    aro.ThrowIfNotCivilized();
+                }
+                catch(CompoundSemanticException)
+                {
+                    // NOP
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void IsCivilizedTest()
+        {
+            foreach (AbstractRousseauObject aro in m_Subjects)
+            {
+                aro.IsCivilized();
+            }
+        }
+
+        /// <summary>
+        ///A test for AbstractRousseauObject Constructor
+        ///</summary>
+        [TestMethod()]
+        public void AbstractRousseauObjectConstructorTest()
+        {
+            AbstractRousseauObject target = new RousseauObject();
+        }
+    }
+}
---
+++ PPWCode.Vernacular.Semantics.Test_I/RousseauObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+using PPWCode.Vernacular.Exceptions.I;
+using PPWCode.Vernacular.Semantics.I;
+
+namespace PPWCode.Vernacular.Semantics.Test_I
+{
+    public class RousseauObject : AbstractRousseauObject
+    {
+        [ContractInvariantMethod]
+        private void TypeInvariants()
+        {
+        }
+
+        public RousseauObject()
+        {
+        }
+
+        public bool MockWild { get; set; }
+
+        public override CompoundSemanticException WildExceptions()
+        {
+            CompoundSemanticException cpe = base.WildExceptions();
+            if (MockWild)
+            {
+                cpe.AddElement(new SemanticException("TEST"));
+            }
+            return cpe;
+        }
+    }
+}
