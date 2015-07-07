121c57c40e232ccbfb075a502c844aee2cef22a0
Nick Joosens <nick_joosens@peopleware.be>
2015-07-07 11:45:10 +0200

    Name changes

---
+++ src/II.Tests/RousseauObject.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2014 by PeopleWare n.v..
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics.Contracts;
+
+using PPWCode.Vernacular.Exception.II;
+using PPWCode.Vernacular.Semantics.II;
+
+namespace PPWCode.Vernacular.Semantics.II.Test
+{
+    public class RousseauObject : AbstractRousseauObject
+    {
+        [ContractInvariantMethod]
+        private void TypeInvariants()
+        {
+        }
+
+        public RousseauObject()
+        {
+        }
+
+        public bool MockWild { get; set; }
+
+        public override CompoundSemanticException WildExceptions()
+        {
+            CompoundSemanticException cpe = base.WildExceptions();
+            if (MockWild)
+            {
+                cpe.AddElement(new SemanticException("TEST"));
+            }
+
+            return cpe;
+        }
+    }
+}
---
+++ src/II.Tests/SemanticObjectC.cs
@@ -0,0 +1,69 @@
+﻿// Copyright 2014 by PeopleWare n.v..
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics.Contracts;
+
+using PPWCode.Vernacular.Semantics.II;
+
+namespace PPWCode.Vernacular.Semantics.II.Test
+{
+    public class SemanticObjectC : AbstractSemanticObject
+    {
+        [ContractInvariantMethod]
+        private void TypeInvariants()
+        {
+            Contract.Invariant(B != null ? B.Cs.Contains(this) : true);
+        }
+
+        public SemanticObjectC()
+        {
+            Contract.Ensures(B == null);
+        }
+
+        private SemanticObjectB m_B;
+
+        public SemanticObjectB B
+        {
+            get { return m_B; }
+
+            set
+            {
+                Contract.Ensures(Contract.OldValue(B) != null && Contract.OldValue(B) != value
+                                     ? !Contract.OldValue(B).Cs.Contains(this) : true);
+                Contract.Ensures(B == value);
+
+                if (m_B != null)
+                {
+                    m_B.RemoveC(this);
+                }
+
+                m_B = value;
+                if (m_B != null)
+                {
+                    m_B.AddC(this);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Closing the loop.
+        /// </summary>
+        public SemanticObjectA A { get; set; }
+
+        public override string LimitedToString()
+        {
+            return "TEST";
+        }
+    }
+}
