cd730d8e2146ac22de4dbbc16d08ae5ec8a51295
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2015-07-12 13:00:20 +0200

    Bumping version for new release.

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -1,14 +1,10 @@
-#region Using
-
 using System.Reflection;
-
-#endregion
 
 [assembly: AssemblyCompany("Peopleware NV")]
 [assembly: AssemblyProduct("PPWCode Vernacular Semantics")]
-[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0.0")]
